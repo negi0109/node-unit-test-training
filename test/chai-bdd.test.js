b5a9ea4e3790baba807e23ff92d812f5cf4c7668
negi0109 <negi0109.seg@gmail.com>
2020-07-02 19:48:42 +0900

    chai bdd 構造体の比較

--- test/chai-bdd.test.js
+++ test/chai-bdd.test.js
@@ -1,4 +1,22 @@
+import { expect } from 'chai';
+
 describe('chai', () => {
     describe('style BDD', () => {
+        describe('構造体比較', () => {
+            const s1 = { a: 1, b: 2 }
+            const s2 = s1
+            const s3 = { a: 1, b: 2 }
+
+            it('同じ構造体', () => {
+                expect(s1).to.equal(s2)
+                expect(s1).to.deep.equal(s2)
+                expect(s1).to.eql(s2)
+            })
+            it('異なる構造体', () => {
+                expect(s1).to.not.equal(s3)
+                expect(s1).to.deep.equal(s3)
+                expect(s1).to.eql(s3)
+            })
+        })
     })
 })
