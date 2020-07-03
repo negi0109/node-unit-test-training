5d3c2f1d780d2f368ff9aa02c5a1555a5b621a73
negi0109 <negi0109.seg@gmail.com>
2020-07-03 19:24:18 +0900

    テスト対象を切り分け

--- test/chai-bdd.test.js
+++ test/chai-bdd.test.js
@@ -4,18 +4,26 @@ describe('chai', () => {
     describe('style BDD', () => {
         describe('構造体比較', () => {
             const s1 = { a: 1, b: 2 }
-            const s2 = s1
-            const s3 = { a: 1, b: 2 }
+            def('s2', () => null)
 
-            it('同じ構造体', () => {
-                expect(s1).to.equal(s2)
-                expect(s1).to.deep.equal(s2)
-                expect(s1).to.eql(s2)
+            describe('同じ構造体', () => {
+                def('s2', () => s1)
+
+                it('equal', () => {
+                    expect(s1).to.equal($s2)
+                    expect(s1).to.deep.equal($s2)
+                    expect(s1).to.eql($s2)
+                })
             })
-            it('異なる構造体', () => {
-                expect(s1).to.not.equal(s3)
-                expect(s1).to.deep.equal(s3)
-                expect(s1).to.eql(s3)
+
+            describe('異なる構造体', () => {
+                def('s2', () => ({ a: 1, b: 2 }))
+
+                it('equal', () => {
+                    expect(s1).to.not.equal($s2)
+                    expect(s1).to.deep.equal($s2)
+                    expect(s1).to.eql($s2)
+                })
             })
         })
     })
